fbb73a47654e863abcd5c6fb0de64abf3feae3ce
Douglas <dougl215@ampt019.slac.stanford.edu>
2023-02-15 16:44:52 -0800

    tweaks to eval, PNN configs

--- NDLArSimReco/loss.py
+++ NDLArSimReco/loss.py
@@ -29,8 +29,24 @@ def NLL_homog(output, truth):
 
 def NLL(output, truth):
     diff = (output - truth).features[:,0]
-    sigma = torch.exp(output.features[:,1])
+    epsilon = 1.e-2
+    sigma = torch.exp(output.features[:,1]) + epsilon
     # sigma = torch.abs(1 + output.features[:,1])
+    print (diff)
+    print (sigma)
+    
+    logp = -0.5*torch.pow(diff/sigma, 2) - torch.log(sigma) # + np.log(np.sqrt(2*np.pi)), ignored
+
+    LL = torch.sum(logp)/len(diff)
+
+    return -LL
+
+def NLL_reluError(output, truth):
+    diff = (output - truth).features[:,0]
+    epsilon = 1.e-2
+    sigma = torch.relu(1.e-2*output.features[:,1]) + epsilon
+    print (diff)
+    print (sigma)
     
     logp = -0.5*torch.pow(diff/sigma, 2) - torch.log(sigma) # + np.log(np.sqrt(2*np.pi)), ignored
 
