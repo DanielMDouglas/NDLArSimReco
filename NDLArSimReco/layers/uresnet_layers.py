fixed layer feature scaling for ResNetEncoder

NDLArSimReco/layers/uresnet_layers.py
@@ -416,7 +416,9 @@ class ResNetEncoder(torch.nn.Module):
             ) 
         )
 
-        self.featureSizesEnc = [(self.nFilters*2**i, self.nFilters*2**(i+1))
+        # self.featureSizesEnc = [(self.nFilters*2**i, self.nFilters*2**(i+1))
+        #                         for i in range(self.depth)]
+        self.featureSizesEnc = [(self.nFilters*(i+1), self.nFilters*(i+2))
                                 for i in range(self.depth)]
         
         self.encoding_layers = []
@@ -434,7 +436,8 @@ class ResNetEncoder(torch.nn.Module):
             )
             self.encoding_blocks.append(
                 ResNetBlock(self.featureSizesEnc[i][1],
-                            self.featureSizesEnc[i][1])
+                            self.featureSizesEnc[i][1],
+                            kernel_size = 3)
             )
         self.encoding_layers = nn.Sequential(*self.encoding_layers)
         self.encoding_blocks = nn.Sequential(*self.encoding_blocks)

NDLArSimReco/loss.py
@@ -389,7 +389,6 @@ class MSE_voxOcc_softmax_masked_totE (loss):
         trueOccupancy = (truth > 0).float()
         
         return nn.functional.binary_cross_entropy(inferredOccupancy, trueOccupancy)
-
     def MSELoss(self, truth, mean, isFilledVoxel):
         truthMask = truth > 0
         maskedMean = mean[truthMask]
@@ -463,14 +462,49 @@ class NLL_voxOcc_softmax_masked_inference (loss):
         return NLL
 
 class CrossEntropy (loss):
+    def __call__(self, output, truth):
+        return self.loss(self.truth_map(truth),
+                         self.feature_map(output))
+
     def feature_map(self, outputTensor):
-        return outputTensor.features[:,0],
+        # return outputTensor.features[:,0],
+        return outputTensor.features
 
     def truth_map(self, truth):
-        return truth.features[:,0]
+        return truth
         
     def loss(self, truth, output):
         return nn.CrossEntropyLoss()(output, truth)
+
+class CrossEntropy_stochastic (loss):
+    """
+    This version of the cross-entropy loss, the output is 
+    interpreted as an array of (N, 2*C) where N is the batch
+    size and C is the number of classes.  For each class,
+    a mean and std are interleaved within the array
+    """
+    throwDist = torch.distributions.normal.Normal(loc = 0,
+                                                  scale = 1)
+    def __call__(self, output, truth):
+        return self.loss(self.truth_map(truth),
+                         self.feature_map(output))
+    def feature_map(self, outputTensor):
+        return outputTensor.features
+    def truth_map(self, truth):
+        return truth
+        
+    def loss(self, truth, output):
+        meanScore = output[:,::2]
+        uncScore = output[:,1::2]
+
+        eps = 1.e-3
+        scale = torch.abs(uncScore) + eps
+
+        throwDir = self.throwDist.rsample(scale.shape).to(device)
+
+        throw = meanScore + throwDir*scale
+
+        return nn.CrossEntropyLoss()(throw, truth)
 
 class semanticSegmentationCrossEntropy (loss):
     def feature_map(self, outputTensor):
@@ -481,9 +515,6 @@ class semanticSegmentationCrossEntropy (loss):
         
     def loss(self, truth, output):
         mask = truth >= 0
-        print (truth[mask])
-        print (output[mask])
-        print (torch.sigmoid(output[mask]))
         return torch.nn.functional.binary_cross_entropy_with_logits(output[mask], truth[mask])
 
 class semanticSegmentationNLL (loss):

NDLArSimReco/network.py
@@ -39,6 +39,7 @@ lossDict = {'NLL': loss.NLL,
             'MSE_voxOcc_softmax_masked_totE': loss.MSE_voxOcc_softmax_masked_totE,
             'NLL_voxOcc_softmax_masked_inference': loss.NLL_voxOcc_softmax_masked_inference,
             'CrossEntropy': loss.CrossEntropy,
+            'CrossEntropy_stochastic': loss.CrossEntropy_stochastic,
             'semanticSegmentationCrossEntropy': loss.semanticSegmentationCrossEntropy,
             'semanticSegmentationNLL': loss.semanticSegmentationNLL,
             'semanticSegmentationNLL_simple': loss.semanticSegmentationNLL_simple,
@@ -429,9 +430,14 @@ class ConfigurableSparseNetwork(ME.MinkowskiNetwork):
             loss = self.criterion(output, truth)
 
             if accuracy:
-                trueLabelMask = truth.features[:,0] >= 0
-                truth = truth.features[trueLabelMask,0]
-                prediction = torch.sigmoid(output.features[trueLabelMask,0]) > 0.5
+                # trueLabelMask = truth.features[:,0] >= 0
+                # truth = truth.features[trueLabelMask,0]
+                # prediction = torch.sigmoid(output.features[trueLabelMask,0]) > 0.5
+                # means = output.features[:,::2]
+                means = output.features
+                print (output.features.shape)
+                prediction = torch.argmax(means, dim = -1)
+                print (prediction.shape)
 
                 thisAccuracy = (sum(prediction == truth)/len(prediction))# .cpu()
                 accList.append(thisAccuracy.item())